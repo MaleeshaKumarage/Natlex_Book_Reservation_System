ec637eed5ccb7e680f137a7780b2bf893d6e005d
MaleeshaKumarage <maleesha.msk@gmail.com>
2023-12-16 12:30:48 +0200

    Project Structure

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection serviceDescriptors)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            serviceDescriptors.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+            serviceDescriptors.AddValidatorsFromAssembly(assembly);
+            return serviceDescriptors;
+        }
+    }
+}
