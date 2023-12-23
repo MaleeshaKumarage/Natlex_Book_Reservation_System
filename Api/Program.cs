b8dc104d1c9ad45167467013c7a238a9a948ed8e
MaleeshaKumarage <maleesha.msk@gmail.com>
2023-12-24 00:45:33 +0200

    Update swagger-ui

--- Api/Program.cs
+++ Api/Program.cs
@@ -7,6 +7,7 @@ using Infrastructure.Repository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Presentation;
 using Serilog;
 using System.Reflection;
@@ -32,6 +33,19 @@ builder.Services.AddSwaggerGen(c =>
 {
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Natlex Book Reservation System API",
+        Version = "v1.0",
+        Description = "This API facilitates the management of a simple book reservation system, offering a set of endpoints to interact with book entities. The CRUD operations for the Book entity include creating, retrieving, updating, and deleting books. Each book is characterized by its unique identifier (id*), title*, author, and additional optional properties.\r\n\r\nAdditionally, the API provides functionality to reserve a book using a POST endpoint, requiring the book's id and a reservation comment as parameters. The reserved status and comment are stored within the system, and if a book is already reserved, the endpoint responds with an appropriate error status.\r\n\r\nFurthermore, the API offers a POST endpoint to remove the reserved status for a given book by passing its id as a parameter.",
+        Contact = new OpenApiContact
+        {
+            Name = "Maleesha Kumarage",
+            Email = "contactmaleesha93@gmail.com",
+            Url = new Uri("https://maleeshakumarage.github.io/"),
+        },
+    });
+
 });
 
 builder.Services.AddDbContext<BookStoreDbContext>(opt => opt.UseInMemoryDatabase(databaseName: "BookStore"));
@@ -51,9 +65,16 @@ builder.Services
     .AddPresentation();
 
 var app = builder.Build();
+app.UseStaticFiles();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+
+    c.DisplayRequestDuration();
+    c.EnableFilter();
+    c.InjectStylesheet("/swagger-custom.css");
+});
 
 app.UseHttpsRedirection();
 
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a62b7a73f34e936f7dffee3765042155c7226c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9c10760ce79f28328bdcab21abee37f859ad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
